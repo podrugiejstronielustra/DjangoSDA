05867b313990981431f313c309d2139058198c43
User <alicjasipa@gmail.com>
2022-04-10 15:30:21 +0200

    methods.html added

--- books/views.py
+++ books/views.py
@@ -42,15 +42,16 @@ def get_arguments_from_query(request: WSGIRequest) -> HttpResponse:
 @csrf_exempt
 def check_http_query_type(request: WSGIRequest) -> HttpResponse:
     query_type = "Unknown"
-    if request.method == "GET":
-        query_type = "This is GET"
-    elif request.method == "POST":
-        query_type = "This is POST"
-    elif request.method == "This is PUT":
-        query_type = "This is PUT"
-    elif request.method == "This is DELETE":
-        query_type = "This is DELETE"
-    return HttpResponse(query_type)
+#    if request.method == "GET":
+#        query_type = "This is GET"
+#    elif request.method == "POST":
+#        query_type = "This is POST"
+#    elif request.method == "This is PUT":
+#        query_type = "This is PUT"
+#    elif request.method == "This is DELETE":
+#        query_type = "This is DELETE"
+#    return HttpResponse(query_type)
+    return render(request, template_name="methods.html", context={})
 
 # 21. Przygotuj funkcję, która zwróci informację o headerach HTTP
 def get_headers(request: WSGIRequest) -> JsonResponse:
