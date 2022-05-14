d1a2ade4909cde0418cfb12e785cc6249b0f254d
User <alicjasipa@gmail.com>
2022-05-14 13:28:20 +0200

    App users added

--- books/models.py
+++ books/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 
 class BookAuthor(models.Model):
     name = models.TextField()
