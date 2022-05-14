a9ea7f473c51391163eb2b566ad31b4613cd608e
User <alicjasipa@gmail.com>
2022-05-14 13:58:01 +0200

    Verification password/hash password added

---
+++ users/utils.py
@@ -0,0 +1,11 @@
+import hashlib
+
+def hash_password_without_salt(password: str) -> str:
+    hash_password = hashlib.sha256(password.encode("ascii")).hexdigest()
+    return hash_password
+
+
+def verification_password_without_salt(password: str, hash_password: str) -> bool:
+    input_password = hashlib.sha256(password.encode("ascii")).hexdigest()
+    return input_password == hash_password
+
