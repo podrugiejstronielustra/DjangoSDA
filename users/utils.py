ec68f2bb44b4a6ff2c227247791e9c38b87298d7
User <alicjasipa@gmail.com>
2022-05-14 14:31:44 +0200

    created hashing password vol.2

--- users/utils.py
+++ users/utils.py
@@ -23,3 +23,11 @@ def hash_password_with_salt(password:str) -> str:
     combined_salt_and_password = ''.join([hashed_password, hashed_salt])
 
     return combined_salt_and_password
+
+def verification_password_with_salt(password: str, hashed_password: str) -> bool:
+    hashed_salt = hashed_password[-64:]
+    new_password = password + hashed_salt
+    password_to_check = hashlib.sha256(new_password.encode("ascii")).hexdigest()
+    combined_salt_and_password = ''.join([password_to_check, hashed_salt])
+
+    return combined_salt_and_password == hashed_password
